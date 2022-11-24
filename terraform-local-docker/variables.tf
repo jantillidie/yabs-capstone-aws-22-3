319c1e34dd41a3b2510fbcfb697e27b6e12c89db
Jan Maaß <git@antipost.de>
2022-11-24 14:56:12 +0100

    Add description and type to tf vars

--- terraform-local-docker/variables.tf
+++ terraform-local-docker/variables.tf
@@ -1,24 +1,40 @@
 variable "MYSQL_ROOT_PASSWORD" {
+  description = "Database root password"
   type = string
+  sensitive   = true
 }
 variable "MYSQL_DATABASE" {
+  description = "Database name"
   type = string
+  sensitive   = true
 }
 variable "MYSQL_USER" {
+  description = "Database user"
   type = string
+  sensitive   = true
 }
 variable "MYSQL_PASSWORD" {
+  description = "Database password"
   type = string
+  sensitive   = true
 }
 variable "WORDPRESS_DB_HOST" {
+  description = "Database container name"
   type = string
+  sensitive   = true
 }
 variable "WORDPRESS_DB_USER" {
+  description = "Database user"
   type = string
+  sensitive   = true
 }
 variable "WORDPRESS_DB_PASSWORD" {
+  description = "Database password"
   type = string
+  sensitive   = true
 }
 variable "WORDPRESS_DB_NAME" {
+  description = "Database name"
   type = string
+  sensitive   = true
 }
